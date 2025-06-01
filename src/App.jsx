3d676403d8d81df230660212c3dee39ee8853b4c
swasty jsy <amekpoagbedaniel.com>
2025-06-01 07:36:14 +0000

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
@@ -8,23 +8,27 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound"; // Import a 404 page component
-
+import Globalstyle from "./styles/Globalstyle"; // Import global styles
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Define your routes here */}
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="bookings" element={<Bookings />} />
-        <Route path="cabins" element={<Cabins />} />
-        <Route path="users" element={<Users />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="account" element={<Account />} />
-        <Route path="login" element={<Login />} /> {/* Fallback route */}
-        <Route path="*" element={<PageNotFound />} /> {/* Default route */}
-        {/* Add more routes as needed */}
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Globalstyle />
+      <BrowserRouter>
+        <Routes>
+          {/* Define your routes here */}
+          <Route index element={<Navigate replace to="Dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} /> {/* Fallback route */}
+          <Route path="*" element={<PageNotFound />} /> {/* Default route */}
+          {/* Add more routes as needed */}
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
