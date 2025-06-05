cbd194d646bed3389fe15f32ea83938e4f2c99ae
daniel amekpoagbe <amekpoagbedaniel@gmail.com>
2025-06-05 23:48:27 +0000

    fetched cabin data using RQ

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,20 +1,19 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import getCabins from "../services/ApiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  useEffect(() => {
-    getCabins().then((cabins) => {
-      console.log("Cabins fetched:", cabins);
-    });
-  }, []);
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter/Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
