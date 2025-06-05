6e87b23e3a211fd7fbe493792662560336866c53
daniel amekpoagbe <amekpoagbedaniel@gmail.com>
2025-06-05 22:07:57 +0000

    addedact query

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import Globalstyle from "./styles/GlobalStyles"; // Import global styles
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
@@ -7,30 +11,41 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound"; // Import a 404 page component
-import Globalstyle from "./styles/GlobalStyles"; // Import global styles
 import AppLayout from "./ui/AppLayout";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000, // 60 seconds
+    },
+  },
+}); // Initialize QueryClient for React Query
+
 const App = () => {
   return (
     <>
-      <Globalstyle />
-      <BrowserRouter>
-        <Routes>
-          {/* Define your main layout here if needed */}
-          <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to="Dashboard" />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="bookings" element={<Bookings />} />
-            <Route path="cabins" element={<Cabins />} />
-            <Route path="users" element={<Users />} />
-            <Route path="settings" element={<Settings />} />
-            <Route path="account" element={<Account />} />
-          </Route>
-          {/* Define your routes here */}
-          <Route path="login" element={<Login />} /> {/* Fallback route */}
-          <Route path="*" element={<PageNotFound />} /> {/* Default route */}
-          {/* Add more routes as needed */}
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Globalstyle />
+        <BrowserRouter>
+          <Routes>
+            {/* Define your main layout here if needed */}
+            <Route element={<AppLayout />}>
+              <Route index element={<Navigate replace to="Dashboard" />} />
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="bookings" element={<Bookings />} />
+              <Route path="cabins" element={<Cabins />} />
+              <Route path="users" element={<Users />} />
+              <Route path="settings" element={<Settings />} />
+              <Route path="account" element={<Account />} />
+            </Route>
+            {/* Define your routes here */}
+            <Route path="login" element={<Login />} /> {/* Fallback route */}
+            <Route path="*" element={<PageNotFound />} /> {/* Default route */}
+            {/* Add more routes as needed */}
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </>
   );
 };
